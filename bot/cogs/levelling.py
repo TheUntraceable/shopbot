e2fb2638f62f4136c40247d8ef262c7fc60b786b
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-08-05 12:57:03 +0100

    Make Database

---
+++ bot/cogs/levelling.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..bot import Bot
+
+class LevellingTheme(commands.Cog): # This is the cog which will handle the "Levelling" theme.
+    def __init__(self, bot: Bot):
+        self.bot = bot
+    
