ad775121e95aaf25b3dba131d5b08a6aa0c27978
flick0 <flickerdroid211@gmail.com>
2022-08-09 08:01:26 +0530

    collect from shop, upgrade menu

---
+++ bot/cogs/upgrade.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+from ..utils.views.upgrade_menu import ShopEdit
+
+class Upgrade(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(name="upgrade", aliases=["up"])
+    async def upgrade(self, ctx):
+        msg = await ctx.reply("getting everything ready for you...")
+        setattr(ctx, "bot_msg", msg)
+        await msg.edit(content="", view=ShopEdit(ctx))
+
+
+async def setup(bot):
+    await bot.add_cog(Upgrade(bot))
